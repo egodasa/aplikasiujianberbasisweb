371132305aad830a31946c447e676ab74e1b6730
egodasa <egodasa@gmail.com>
2017-09-27 19:53:58 +0700

    Penambahan properti banyak peserta dan banyak soal menggunakan subquery pada knex

--- routes/ujian.js
+++ routes/ujian.js
@@ -12,7 +12,10 @@ router.get('/:id?', (req, res, next) => {
 	var op = null;
 	if(id == 0) op = "!=";
 	else op = "=";
-	db('tbujian').select().limit(limit).offset(offset).where('id',op,id).
+	var field_ujian = db.raw('??',['tbujian.id']);
+	var subquery1 = db('tbpeserta_ujian').count('tbpeserta_ujian.id_peserta').where('tbpeserta_ujian.id_ujian',field_ujian).as('banyak_peserta');
+	var subquery2 = db('tbsoal_ujian').count('tbsoal_ujian.id_soal').where('tbsoal_ujian.id_ujian',field_ujian).as('banyak_soal');
+	db('tbujian').select('*',subquery1,subquery2).limit(limit).offset(offset).where('id',op,id).
 	then(function(rows){
 		hasil.status = true;
 		hasil.data = rows;
@@ -127,27 +130,28 @@ router.put('/:id', (req, res, next) => {
 var checkDataPeserta = require('../validator/peserta_ujian/create_update');
 router.get('/:id/peserta/:idPeserta?', (req, res, next) => {
     var id = req.params.id;
-    var idPeserta = req.params.idPeserta || 0;
-    var limit = req.query.limit || 0;
-    var offset = req.query.offset || 0;
+    var limit = 1*req.query.limit || null;
+    var offset = 1*req.query.offset || null;
     var belumDitambahkan = req.query.belumDitambahkan || 0;
-    if(belumDitambahkan == 0){
-		sql = 'call getPesertaUjian(?,?,?,?);';
-		sqlParam = [id,idPeserta,limit,offset];
-	}else{
-		sql = 'call getNotPesertaUjian(?,?,?);';
-		sqlParam = [id,limit,offset];
-	}
-    koneksi.query(sql,sqlParam, (e, r, f) => {
-        var hasil = {};
-        if (!e) hasil.status = true;
-        else hasil.status = false;
-        hasil.data = r[0];
-        hasil.row = r[1][0].jumlah;
-        hasil.error = e;
-        res.json(hasil);
+    var hasil = {};
+    db('tbpeserta').limit(limit).offset(offset).join('tbpeserta_ujian','tbpeserta.id','tbpeserta_ujian.id_peserta').
+    select('tbpeserta.id as id','tbpeserta.nm_peserta as nm_peserta').where('tbpeserta_ujian.id_ujian',id)
+    .then((rows)=>{
+		hasil.status = true;
+		hasil.data = rows;
+		hasil.current_row = rows.length;
+		return db('tbpeserta').join('tbpeserta_ujian','tbpeserta.id','tbpeserta_ujian.id_peserta').count('tbpeserta_ujian.id_peserta as jumlah').where('tbpeserta_ujian.id_ujian',id);
+		})
+	.then((jumlah)=>{
+		hasil.row = jumlah[0].jumlah;
+		res.json(hasil);
+		})
+	.catch((err)=>{
+		hasil.status = false;
+		hasil.error = err;
+		res.json(err);
+		});
     });
-});
 router.post('/:id/peserta/', (req, res, next) => {
     var data = req.body;
     var id = req.params.id;
@@ -170,57 +174,63 @@ router.post('/:id/peserta/', (req, res, next) => {
             };
             res.json(hasil);
         } else {
-            sql = 'call createPesertaUjian(?,?);';
-            console.log(sql);
-            koneksi.query(sql,[id,data.id_peserta], function(e, r, f) {
-                var hasil = {};
-                if (!e) hasil.status = true;
-                else hasil.status = false;
-                hasil.error = e;
-                res.json(hasil);
-            });
+            db('tbpeserta_ujian').insert(data)
+            .then(()=>{
+				hasil.status = true;
+				res.send(ha4sil);
+				})
+			.catch((err)=>{
+				hasil.status = true;
+				hasil.error = err;
+				res.send(hasil);
+				});
         }
     });
 });
 router.delete('/:id/peserta/:id_pu', (req, res, next) => {
-    var id = req.params.id;
-    var id_pu = req.params.id_pu;
-    var id_peserta_ujian = req.body.id_peserta_ujian;
-    console.log(id_peserta_ujian);
+    var id_ujian = req.params.id;
+    var id_peserta = req.params.id_pu;
     var hasil = {};
-    sql = 'call deletePesertaUjian(?);';
-    koneksi.query(sql,[id_pu], (e, r, f) => {
-        var hasil = {};
-        if (!e) hasil.status = true;
-        else hasil.status = false;
-        hasil.error = e;
-        res.json(hasil);
-    });
+    db('tbpeserta_ujian').where('id_peserta',id_peserta).whereAnd('id_ujian',id_ujian).del()
+    .then(()=>{
+		hasil.status = true;
+		res.json(hasil);
+		})
+	.catch((err)=>{
+		hasil.status = false;
+		hasil.error = err;
+		res.json(hasil);
+		});
 });
 
 //Soal Ujian
 
 router.get('/:id/soal', (req, res, next) => {
     var id = req.params.id;
-    var limit = req.query.limit || 0;
-    var offset = req.query.offset || 0;
+    var limit = 1*req.query.limit || null;
+    var offset = 1*req.query.offset || null;
     var belumDitambahkan = req.query.belumDitambahkan || 0;
-	sql = 'call getSoalUjian(?,?,?);';
-	sqlParam = [id,limit,offset];
-    koneksi.query(sql,sqlParam, (e, r, f) => {
-        var hasil = {};
-        if (!e) hasil.status = true;
-        else hasil.status = false;
-        console.log(r);
-        hasil.data = r[0];
-        hasil.row = r[1][0].jumlah;
-        hasil.error = e;
-        res.json(hasil);
-    });
+    var hasil = {};
+    db('tbsoal').limit(limit).offset(offset).join('tbsoal_ujian','tbsoal.id','tbsoal_ujian.id_soal').
+    select('tbsoal.id as id','tbsoal.isi_soal as isi_soal').where('tbsoal_ujian.id_ujian',id)
+    .then((rows)=>{
+		hasil.status = true;
+		hasil.data = rows;
+		hasil.current_row = rows.length;
+		return db('tbsoal').join('tbsoal_ujian','tbsoal.id','tbsoal_ujian.id_soal').count('tbsoal_ujian.id_soal as jumlah').where('tbsoal_ujian.id_ujian',id);
+		})
+	.then((jumlah)=>{
+		hasil.row = jumlah[0].jumlah;
+		res.json(hasil);
+		})
+	.catch((err)=>{
+		hasil.status = false;
+		hasil.error = err;
+		res.json(err);
+		});
 });
 router.post('/:id/soal/', (req, res, next) => {
 	var data = req.body;
-	var id = req.params.id;
 	var hasil = {};
 	console.log(data);
 	req.checkBody(checkDataSoal);
@@ -248,36 +258,49 @@ router.post('/:id/soal/', (req, res, next) => {
 	res.json(hasil); 
 	}
 	else{
-	var sql1 = '';
-	var pg = data.pilihanGanda;
-	for(var x=0;x<pg.length;x++){
-		if(x == 0) sql1 = '(@id_pg,"'+pg[x].huruf+'","'+pg[x].isi_pilihan+'")';
-		else sql1+= ',(@id_pg,"'+pg[x].huruf+'","'+pg[x].isi_pilihan+'")';
-	}
-	sql = 'set @id_pg=genIdSoal(); call createSoal(@id_pg,?,?); insert into tbpilihan_ganda (id_soal,huruf,isi_pilihan) values '+sql1+'; call createSoalUjian(?,@id_pg);';
-	koneksi.query(sql,[data.isi_soal,data.jawaban,id], function(e, r, f){
-		if(!e) hasil.status = true;
-		else hasil.status = false;
-		hasil.error = e;
-		console.log(hasil);
+	db('tbsoal').returning('id').insert({
+		isi_soal : data.isi_soal,
+		jawaban : data.jawaban
+		}).
+	then(function(id){
+		var y = data.pilihanGanda.length;
+		for(x=0;x<y;x++){
+			data.pilihanGanda[x].id_soal = id;
+		}
+		return db('tbpilihan_ganda').returning('id_soal').insert(data.pilihanGanda);
+		}).
+	then((id)=>{
+		console.log(id);
+		return db('tbsoal_ujian').insert({id_ujian : id,id_soal:id_soal});
+		}).
+	then(function(){
+		hasil.status =true;
+		hasil.error = null;
+		res.send(hasil);
+		}).
+	catch(function(err){
+		hasil.status = false;
+		hasil.error = err;
 		res.json(hasil);
 		});
 	}
-});
+	});
 });
 router.delete('/:id/soal/:idSoal', (req, res, next) => {
     var id = req.params.id;
-    var idSoal = req.params.idSoal;
+    var id_soal = req.params.idSoal;
     var data = req.body;
     var hasil = {};
-    sql = 'call deleteSoalUjian(?);';
-    koneksi.query(sql,[idSoal], (e, r, f) => {
-        var hasil = {};
-        if (!e) hasil.status = true;
-        else hasil.status = false;
-        hasil.error = e;
-        res.json(hasil);
-    });
+    db('tbsoal_ujian').where({id_soal : id_soal,id_ujian:id}).del()
+    .then(()=>{
+		hasil.status = true;
+		res.json(hasil);
+		})
+	.catch((err)=>{
+		hasil.status = false;
+		hasil.error = err;
+		res.json(hasil);
+		});
 });
 
 //HASIL UJIAN
