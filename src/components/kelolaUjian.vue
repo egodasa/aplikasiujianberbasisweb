3d12e96f28dd8dfe1f00dbaab491675f7fcff595
egodasa <egodasa@gmail.com>
2017-10-14 23:38:20 +0700

    Perbaikan component peserta dan ujian untuk menggunakan arrow function

--- src/components/kelolaUjian.vue
+++ src/components/kelolaUjian.vue
@@ -3,17 +3,19 @@
     <div class="w3-container w3-blue-gray w3-round">
         <h2>Daftar Ujian</h2>
     </div>
+    <gen-form :url="url" :input="listForm"></gen-form>
     <gen-table :url="url" :header="tableHeader"></gen-table>
     </div>
 </template>
 
 <script>
 import genTable from './GenTable.vue'
+import genForm from './formGenerator.vue'
 
 export default {
   name: 'kelolaUjian',
   components : {
-      genTable
+      genTable, genForm
   },
   data () {
       return {
