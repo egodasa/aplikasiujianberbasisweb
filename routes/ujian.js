a4d2fe415a8a180bdaaf95a714724fae2d2c90ba
egodasa <egodasa@gmail.com>
2018-01-03 22:13:15 +0700

    Fix desain export excel

--- routes/ujian.js
+++ routes/ujian.js
@@ -6,7 +6,9 @@ var axios = require('axios')
 const excel = require('node-excel-export');
 _ = require('lodash')
 var Excel = require('exceljs');
-
+var formatWaktu = require('date-fns/format')
+var lokalisasi = require('date-fns/locale/id')
+let tmp_file = require('tempfile')
 router.get('/:id?', (req, res, next) => {
     var id = req.params.id || 0;
     var nidn = req.query.nidn || 0;
@@ -429,23 +431,72 @@ router.get('/:idUjian/hasil/cetak/csv', (req, res, next) => {
 router.get('/:idUjian/hasil/cetak/excel', (req, res, next) => {
     let id_ujian = req.params.idUjian
     var workbook = new Excel.Workbook();
-    var worksheet = workbook.addWorksheet('Laporan Ujian');
+    var worksheet = workbook.addWorksheet('Laporan Hasil Ujian');
     db('lap_ujian').select("*", db.raw("concat(to_char(hari,'dd TMMonth yyyy'),' ',mulai,'-',selesai) as ket_waktu")).where('id_ujian',id_ujian)
         .then((rows)=>{
             var hasil = rows[0]
-            worksheet.addRow(['Mata Kuliah',hasil.nm_matkul,'Dosen',hasil.nm_dosen])
-            worksheet.addRow(['Kelas',hasil.nm_kelas.toString(),'Tahun Akademik',hasil.tahun_akademik])
-            worksheet.addRow(['Hari',hasil.ket_waktu])
-            worksheet.addRow(['Jenis Ujian',hasil.nm_jujian,'Jenis Soal',hasil.nm_jsoal])
+            worksheet.addRow(['LAPORAN HASIL UJIAN'])
+            worksheet.addRow(['Mata Kuliah',hasil.nm_matkul])
+            worksheet.addRow(['Dosen',hasil.nm_dosen])
+            worksheet.addRow(['Kelas',hasil.nm_kelas.toString()])
+            worksheet.addRow(['Tahun Akademik',hasil.tahun_akademik])
+            worksheet.addRow(['Hari',formatWaktu(new Date(hasil.hari), 'DD MMMM YYYY', {locale : lokalisasi})])
+            worksheet.addRow(['Jam',hasil.mulai+' - '+hasil.selesai])
+            worksheet.addRow(['Jenis Ujian',hasil.nm_jujian])
+            worksheet.addRow(['Jenis Soal',hasil.nm_jsoal])
             return db(db.raw("getHasilUjian('" + id_ujian + "')")).select(db.raw('distinct(nobp),id_ujian,nm_mahasiswa,id_kelas,nm_kelas,nilai,status_ujian_peserta,nm_status_ujian_peserta')).where('id_ujian', id_ujian).orderBy('nobp','asc')
         })
         .then(function (rows) {
+            var fontHeader = {
+                size : 12,
+                bold : true
+            }
+            var fillHeader = {
+                type: 'pattern',
+                pattern:'solid',
+                fgColor:{argb:'00DFE2E5'}
+            }
+            worksheet.addRow([])
             worksheet.addRow(['NOBP','Nama','Kelas','Status','Nilai'])
             _.forEach(rows,(v,k) => {
                 worksheet.addRow([v.nobp,v.nm_mahasiswa,v.nm_kelas,v.nm_status_ujian_peserta,v.nilai])
             });
-            workbook.xlsx.writeFile('gundulmu.xlsx').then(()=>{
-                res.download('./gundulmu.xlsx',id_ujian+'.xlsx')
+            //style
+            worksheet.getRow(11).alignment = {vertical : "middle",horizontal: "center"}
+            for(var x=2;x<=9;x++){
+                worksheet.mergeCells('B'+x+':E'+x)
+                worksheet.getCell('A'+x).font = fontHeader
+            }
+            worksheet.mergeCells('A1:E1')
+            worksheet.getCell('A1').alignment = {vertical : "middle",horizontal: "center"}
+            worksheet.getCell('A1').font = {
+                size : 17,
+                bold : true
+            }
+            worksheet.getCell('A11').font = fontHeader
+            worksheet.getCell('B11').font = fontHeader
+            worksheet.getCell('C11').font = fontHeader
+            worksheet.getCell('D11').font = fontHeader
+            worksheet.getCell('E11').font = fontHeader
+            worksheet.getCell('A11').fill = fillHeader
+            worksheet.getCell('B11').fill = fillHeader
+            worksheet.getCell('C11').fill = fillHeader
+            worksheet.getCell('D11').fill = fillHeader
+            worksheet.getCell('E11').fill = fillHeader
+            
+            for(var x = 2;x <= 9;x++){
+                worksheet.getCell('A'+x).font = fillHeader
+            }
+            worksheet.getCell('A1').border = {
+                top: {style:'thin'},
+                left: {style:'thin'},
+                bottom: {style:'thin'},
+                right: {style:'thin'}
+            };
+            //enof style
+            var file_tmp = tmp_file('.xlsx')
+            workbook.xlsx.writeFile(file_tmp).then(()=>{
+                res.download(file_tmp,id_ujian+'.xlsx')
             })
         })
         .catch(function (err) {
